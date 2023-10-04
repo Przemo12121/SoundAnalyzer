8d36eb3f1a7e85ec8b1719cb504e8bd5c27122f0
Przemo12121 <przemyslaw.malecki98@gmail.com>
2023-10-05 00:20:52 +0200

    found error in model merging

--- TFlite/train_model_2.py
+++ TFlite/train_model_2.py
@@ -5,7 +5,8 @@ import numpy as np
 from sklearn.preprocessing import MultiLabelBinarizer
 
 classes = getClasses("classes.csv")
-
+# xyz = tfHub.load("https://tfhub.dev/google/yamnet/1")
+# print(baseModel(np.zeros(16000))[0][0])
 def x(filename, label):
     print(label)
     return getData(filename), label
@@ -28,36 +29,60 @@ def prepareDataset(pathToLabels: str):
 trainingDataset = prepareDataset("labels_training.csv")
 validationDataset = prepareDataset("labels_validation.csv")
 
-feature_extractor_layer = tfHub.KerasLayer(
+baseModel = tfHub.KerasLayer(
     "https://tfhub.dev/google/yamnet/1",
+    input_shape=(),
+    # output_shape=(521, 1024, 64),
     trainable=False)
+
 input = tf.keras.layers.Input(shape=(), name="Input", dtype=tf.float32)
-net = feature_extractor_layer(input)
-# output = tf.keras.layers.concatenate([net[0], net[1], net[2]])
-output = tf.keras.layers.Dense(4, activation="softmax")(net[0])
-model = tf.keras.Model(input, output)
+net = baseModel(input)
+baseModelWrapped = tf.keras.Model(input, net)
 
-## OUTPUT LAYER RETURNS WRONG SHAPE
+model = tf.keras.Sequential([
+    tf.keras.layers.Input(shape=(None,521)),
+    # tf.keras.layers.Average(),
+    tf.keras.layers.Dense(3, activation="softmax"),
+])
 
+# yamnet model outputs shape (input_length/8000,521), required is to average along axis=0 before added layer
+
+# x = tfHub.load("https://tfhub.dev/google/yamnet/1")
+# print(x(np.zeros(32000))[0].numpy().mean(axis=0).shape)
+# def narrowOutput(output):
+#     return output[0]
+
+# outputNarrowed = narrowOutput(baseModel.output)
+# output = model(outputNarrowed)
+# model = tf.keras.Model(baseModel.input, output)
 # model.summary()
-for f, l in trainingDataset.take(1):
-    print("Shape of features array:", f.numpy().flatten())
-    print("Shape of labels array:", l)
-model.compile(
-    optimizer="adam",
-    loss="categorical_crossentropy",
-    metrics=["accuracy"],
-    run_eagerly=True
-)
 
-# # print(getData("./data_training/silence/20.wav"))
-# # model.predict(getData("./data_training/silence/20.wav")[0])
 
-model.fit(
-    trainingDataset,
-    validation_data=validationDataset,
-    epochs=10,
-    shuffle=False
-)
+# ## OUTPUT LAYER RETURNS WRONG SHAPE
 
-# # model.save("models/dummy")
+# for f, l in trainingDataset.take(1):
+#     print("Shape of features array:", f.numpy().flatten())
+#     print("Shape of labels array:", l)
+
+# logits = baseModel(np.zeros(8000))
+# print(logits)
+
+# model.compile(
+#     optimizer="adam",
+#     loss="sparse_categorical_crossentropy",
+#     metrics=["accuracy"],
+#     run_eagerly=True
+# )
+
+# # # print(getData("./data_training/silence/20.wav"))
+# # # model.predict(getData("./data_training/silence/20.wav")[0])
+
+# model.fit(
+#     trainingDataset,
+#     validation_data=validationDataset,
+#     epochs=10,
+#     shuffle=False
+# )
+
+# # # model.save("models/dummy")
+# https://stackoverflow.com/questions/59381246/how-to-sequentially-combine-2-tensorflow-models
